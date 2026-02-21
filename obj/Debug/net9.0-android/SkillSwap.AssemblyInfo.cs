3a0695c10e569f3ea9e2419c056c1326657aa45e
yovanynahuat-admin <cyovanyemmanuel@gmail.com>
2026-02-21 01:15:27 -0600

    Agregado de sistema de login y registro con guardado JSON y navegacion entre vistas

--- obj/Debug/net9.0-android/SkillSwap.AssemblyInfo.cs
+++ obj/Debug/net9.0-android/SkillSwap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkillSwap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b796b202c83ecb23e565357df4564bb40623ad49")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkillSwap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkillSwap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
